c0e96a24ad287b7fb8f7b612600807cb8e1f97d8
pubranko <mikuras3@outlook.com>
2023-08-08 20:25:27 +0900

    不要となったflowの削除

--- app/prefect_lib/exec/daily_clear_exec.py
+++
@@ -1,2 +0,0 @@
-from prefect_lib.flows.daily_clear_flow import daily_clear_flow
-daily_clear_flow()
--- app/prefect_lib/exec/monthly_clear_exec.py
+++
@@ -1,4 +0,0 @@
-from prefect_lib.flows.monthly_clear_flow import monthly_clear_flow
-
-
-monthly_clear_flow()
