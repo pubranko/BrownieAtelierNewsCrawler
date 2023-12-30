438b6fa77d6ba1af642b3e06e2edcca417be8b0f
pubranko <mikuras3@outlook.com>
2023-12-30 17:48:05 +0900

    pipenvの仮想環境ディレクトリ見直し。 ローカルテスト用docker-composeのネットワーク対応。

--- sh/prefect_agent_start.sh
+++ sh/prefect_agent_start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # python仮想環境を有効化
-cd $HOME/BrownieAtelier/app
-# docker内では使えない。「pipenv run 」を付けることでpipenvの仮想環境内のコマンドが使用可能となる。
-# python3 -m pipenv shell
+cd $HOME/BrownieAtelier/
+. $HOME/.venv/bin/activate
 
 # prefect APIの向き先を指定生成
-pipenv run prefect config set PREFECT_API_URL=$PREFECT__API_URL
+prefect config set PREFECT_API_URL=$PREFECT__API_URL
 
 # prefectクラウドへのログイン
-pipenv run prefect cloud login --key $PREFECT__API_KEY --workspace $PREFECT__WORK_SPACE
-pipenv run prefect cloud logout
-pipenv run prefect cloud login --key $PREFECT__API_KEY --workspace $PREFECT__WORK_SPACE
+prefect cloud login --key $PREFECT__API_KEY --workspace $PREFECT__WORK_SPACE
+prefect cloud logout
+prefect cloud login --key $PREFECT__API_KEY --workspace $PREFECT__WORK_SPACE
 
 # prefectエージェント起動
-pipenv run prefect agent start --pool brownie-atelier-agent-pool
+cd $HOME/BrownieAtelier/app
+prefect agent start --pool brownie-atelier-agent-pool
 
