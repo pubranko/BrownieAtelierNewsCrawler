45fdd09cfe1e1d4d5df086902cd3405f7fff506e
pubranko <mikuras3@outlook.com>
2024-04-29 23:18:31 +0900

    re

--- sh/github_actions_unit_test.sh
+++ sh/github_actions_unit_test.sh
@@ -8,5 +8,6 @@ which python
 cd app
 # python prefect_lib/test/github_actions_unit_test.py
 # python prefect_lib/github_actions_unit_tests/github_actions_unit_test_01.py
-python $UNIT_TEST_FILE
+echo "$UNIT_TEST_FILE"
+python "$UNIT_TEST_FILE"
 
