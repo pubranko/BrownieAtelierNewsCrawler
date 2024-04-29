1d36d229082c054026d0d9157ff389faad235366
pubranko <mikuras3@outlook.com>
2024-04-29 22:39:55 +0900

    re

--- sh/github_actions_unit_test.sh
+++ sh/github_actions_unit_test.sh
@@ -6,4 +6,7 @@ which python
 
 # テスト用のシェルを起動
 cd app
-python prefect_lib/test/github_actions_unit_test.py
+# python prefect_lib/test/github_actions_unit_test.py
+# python prefect_lib/github_actions_unit_tests/github_actions_unit_test_01.py
+python $UNIT_TEST_FILE
+
