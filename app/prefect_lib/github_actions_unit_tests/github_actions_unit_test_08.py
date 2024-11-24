289966d01e89948a01adadbc730daa09fbc682d5
pubranko <mikuras3@outlook.com>
2024-11-24 18:13:42 +0900

    morning_flow_netに引数を追加しテストしやすいように見直し

--- app/prefect_lib/github_actions_unit_tests/github_actions_unit_test_08.py
+++ app/prefect_lib/github_actions_unit_tests/github_actions_unit_test_08.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta
 from prefect.testing.utilities import prefect_test_harness
+from shared.settings import TIMEZONE
 
 def test_exec():
     with prefect_test_harness():
@@ -14,7 +16,8 @@ def test_exec():
         from prefect_lib.flow_nets.morning_flow_net import morning_flow_net
 
         # 絞り込み用の引数がなければ全量チェック
-        morning_flow_net()
+        base_datetime = datetime.now().astimezone(TIMEZONE).replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)  # 当日の現在時＋１・０分・０秒
+        morning_flow_net(base_datetime)
 
 if __name__ == "__main__":
     test_exec()
--- app/prefect_lib/local_test_exec/morning_flow_net_exec.py
+++ app/prefect_lib/local_test_exec/morning_flow_net_exec.py
@@ -1,11 +1,17 @@
+from datetime import datetime, timedelta
 from prefect_lib.flow_nets.morning_flow_net import morning_flow_net
 from prefect.testing.utilities import prefect_test_harness
+from shared.settings import TIMEZONE
 
 def test_exec():
     with prefect_test_harness():
 
         # 絞り込み用の引数がなければ全量チェック
         morning_flow_net()
+
+        morning_flow_net(
+            datetime(2024, 10, 20, 0, 0, 0, 000000).astimezone(TIMEZONE)
+        )
     
 if __name__ == "__main__":
     test_exec()
