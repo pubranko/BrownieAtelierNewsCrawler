91a3056b3aada2379b34489fd02d1351a4fba51f
pubranko <mikuras3@outlook.com>
2025-07-22 23:16:31 +0900

    PREFECT_RUN_SCRIPTで実際に実行するPrefectFlowを指定する方式へ修正し、github-actionsでテスト用のフローが実行されない不具合を修正。

--- sh/prefect_run_and_server_start.sh
+++ sh/prefect_run_and_server_start.sh
@@ -10,6 +10,7 @@ echo "Current dir: $PWD"
 echo "Prefect home: $PREFECT_HOME"
 echo "API URL: $PREFECT_API_URL"
 echo "Work pool: $PREFECT__WORK_POOL"
+echo "Run script: $PREFECT_RUN_SCRIPT"
 
 # Prefect Serverをバックグラウンドで起動
 prefect server start &
@@ -20,4 +21,5 @@ sleep $SLEEP_TIME_AFTER_PREFECT_SERVER_STARTUP
 cd app
 echo "フロー実行"
 export PYTHONPATH=$PWD:$PYTHONPATH
-python prefect_lib/batch_exec/starter.py
+# python prefect_lib/batch_exec/starter.py
+python $PREFECT_RUN_SCRIPT
